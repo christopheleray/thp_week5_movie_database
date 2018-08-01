0739555e46d92b3aad25682f8912762c0f652f82
christopheleray <christophe.leray1@gmail.com>
2018-08-01 14:31:47 +0200

    add model director and remove director colmun

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,14 @@
 require 'faker'
 Movie.delete_all
 
+5.times do
+director = Director.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+
 5.times do 
- movie =  Movie.create(title: Faker::Superhero.name, release_year: Faker::Number.between(1900, 2018), director: Faker::Superhero.power)
+ movie =  Movie.create(
+  title: Faker::Superhero.name, 
+  release_year: Faker::Number.between(1900, 2018),
+  director_id: rand((Director.first.id)..(Director.last.id))
+  )
 end
